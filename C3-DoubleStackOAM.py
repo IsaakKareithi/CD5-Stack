7a8d03ce5f4b82f64b4708787c56202816d39ca5
isaakKareithi <isaacsgk2@gmail.com>
2025-11-04 18:11:13 +0300

    C3-DoubleStackOAM.py

---
+++ C3-DoubleStackOAM.py
@@ -0,0 +1,58 @@
+#python scrip to implement two stack in a list
+class twoStacks:
+
+    def __init__(self, n):
+        self.size = n 
+        self.ar = [None] * n
+        self.top1 = -1
+        self.top2 = self.size
+
+    def push1(self, x):
+
+        if self.top1 < self.top2 - 1:
+            self.top1 = self.top1 + 1
+            self.ar[self.top1] = x
+
+        else:
+            print("Stack Overflow")
+            exit(1)
+
+    def push2(self, x):
+
+        if self.top1 < self.top2 - 1:
+            self.top2 = self.top2 - 1
+            self.ar[self.top2] = x
+        else:
+            print("Stack Overflow")
+            exit(1)
+
+    def pop1(self):
+        if self.top1 >= 0: 
+            x = self.ar[self.top1]
+            self.top1 = self.top1 - 1
+            return x
+        else:
+            print("Stack Underflow")
+            exit(1)
+
+    def pop2(self):
+        if self.top2 < self.size:
+            x = self.ar[self.top2]
+            self.top2 = self.top2 + 1
+            return x
+        else:
+            print("Stack Underflow")
+            exit()
+
+
+#main program
+ts = twoStacks(5)
+ts.push1(5)
+ts.push2(10)
+ts.push2(16)
+ts.push1(13)
+ts.push2(6)
+
+print("Popped element from stack1 is: " + str(ts.pop1()))
+ts.push2(12)
+print("Popped element from stack2 is: " + str(ts.pop2()))
